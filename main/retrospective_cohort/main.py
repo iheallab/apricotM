4257e3df72404cc2223de3896b5fdac36f8f468d
Miguel Contreras <148377434+contrerasmiguel98@users.noreply.github.com>
2024-03-16 18:27:44 -0400

    Update main.py

--- main/retrospective_cohort/main.py
+++ main/retrospective_cohort/main.py
@@ -4,15 +4,10 @@ from variables import time_window
 
 print(f"Preparing {time_window}h window data.")
 
-# scripts = [
-#     "1_filter_split_cohort.py",
-#     "2_build_static.py",
-#     "3_build_seq.py",
-#     "4_outcomes.py",
-#     "5_build_hdf5.py",
-# ]
-
 scripts = [
+    "1_filter_split_cohort.py",
+    "2_build_static.py",
+    "3_build_seq.py",
     "4_outcomes.py",
     "5_build_hdf5.py",
 ]
