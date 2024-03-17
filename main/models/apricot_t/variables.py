dfe3e0b16d1382d0a2a4cf695438465c7ca94221
Miguel Contreras <148377434+contrerasmiguel98@users.noreply.github.com>
2024-03-16 21:30:32 -0400

    Update variables.py

--- main/models/apricot_t/variables.py
+++ main/models/apricot_t/variables.py
@@ -3,4 +3,4 @@ time_window = 4
 OUTPUT_DIR = f"{HOME_DIR}/deepacu/main/{time_window}h_window"
 DATA_DIR = f"{HOME_DIR}/deepacu/main/prospective_cohort/{time_window}h_window"
 model = "apricott"
-MODEL_DIR = f"/home/contreras.miguel/deepacu/main/{time_window}h_window/model/{model}"
+MODEL_DIR = f"{HOME_DIR}/deepacu/main/{time_window}h_window/model/{model}"
